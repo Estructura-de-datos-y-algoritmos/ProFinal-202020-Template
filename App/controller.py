7259d9402b2d8d129cc27ce0fc9a93666ebd9cc8
Nikolas-1 <69609991+Nikolas-1@users.noreply.github.com>
2020-12-13 20:33:07 -0500

    Parte Final

--- App/controller.py
+++ App/controller.py
@@ -27,6 +27,7 @@
 import config as cf
 from App import model
 import csv
+import os
 
 """
 El controlador se encarga de mediar entre la vista y el modelo.
@@ -40,12 +41,80 @@ recae sobre el controlador.
 #  Inicializacion del catalogo
 # ___________________________________________________
 
+def init():
+    analyzer = model.newAnalyzer()
+    return analyzer
 
 # ___________________________________________________
 #  Funciones para la carga de datos y almacenamiento
 #  de datos en los modelos
 # ___________________________________________________
 
+
+def loadFile(analyzer):
+    
+    for filename in os.listdir(cf.data_dir):
+        if filename.endswith('.csv'):
+            print('Cargando archivo: ' + filename)
+            loadData(analyzer, filename)
+    
+    model.actualizaPuntos(analyzer)
+    model.actulizarRuta(analyzer)
+    #model.muestradatos(analyzer)
+    return analyzer
+
+
+def loadData(analyzer, tripFile):
+    """
+    Carga los datos de los archivos CSV en el modelo
+    """
+    tripFile = cf.data_dir + tripFile
+    input_file = csv.DictReader(open(tripFile, encoding="utf-8"),
+                                delimiter=",")
+    i = 0
+    for trip in input_file:
+        i = i+1
+        model.addTrip(analyzer,trip)
+    
+    print("totla i***********: ",i)
+        
+    return analyzer
+
+        
+  
+
 # ___________________________________________________
 #  Funciones para consultas
 # ___________________________________________________
+
+
+def ordenar(analyzer,valcampo):
+    return model.ordenar(analyzer, valcampo)
+
+
+def puntosPorFecha(analyzer, fecha,numero):
+    return model.puntosPorFecha(analyzer, fecha,numero)
+
+
+def puntosPorRangoFecha(analyzer, fecha1,fecha2, numero):
+    return model.puntosPorRangoFecha(analyzer, fecha1,fecha2, numero)
+
+
+def minimumCostPaths(analyzer, initialStation):
+    """
+    Calcula todos los caminos de costo minimo de initialStation a todas
+    las otras estaciones del sistema
+    """
+    return model.minimumCostPaths(analyzer, initialStation)
+
+
+def minimumCostPath(paths, destStation):
+    """
+    Retorna el camino de costo minimo desde initialStation a destStation
+    """
+    return model.minimumCostPath(paths, destStation)
+
+
+
+def rutasPorRangoHora(analyzer, h_ini,h_fin, ruta):
+    return model.rutasPorRangoHora(analyzer, h_ini,h_fin,ruta)
--- App/view.py
+++ App/view.py
@@ -29,8 +29,14 @@ import sys
 import config
 from App import controller
 from DISClib.ADT import stack
+from time import process_time
 import timeit
 assert config
+
+
+
+from DISClib.ADT import list as lt
+from DISClib.DataStructures import listiterator as it
 
 """
 La vista se encarga de la interacción con el usuario.
@@ -51,3 +57,149 @@ operación seleccionada.
 """
 Menu principal
 """
+
+
+def printMenu():
+    print("\n")
+    print("*******************************************")
+    print("Bienvenido")
+    print("1- Inicializar Analizador")
+    print("2- Cargar información de Taxis")
+    print("3- Requerimiento 1")
+    print("4- Requerimiento 2a")
+    print("5- Requerimiento 2b")
+    print("6- Requerimiento 3")
+    print("0- Salir")
+    print("*******************************************")
+
+
+while True:
+    printMenu()
+    inputs = input('Seleccione una opción para continuar\n')
+
+    if int(inputs[0]) == 1:
+        print("\nInicializando....")
+        # cont es el controlador que se usará de acá en adelante
+        cont = controller.init()
+
+    elif int(inputs[0]) == 2:
+        print("\nCargando información de Rutas Taxis ....")
+        t1 = process_time()
+
+        controller.loadFile(cont) 
+
+        print("Muestar carga")
+        t2 = process_time()
+        t = t2-t1
+        print("Tiempo requerido: ", t)
+
+
+  
+    elif int(inputs[0]) == 3:
+        m = int(input("Top M compañias por texis Afiliados: "))
+        n = int(input("Top N compañias por Servicios Realizados: "))
+
+        print("Numero Total de Taxis : ", cont['totalTaxi'])
+        print("Numero Total de compañias : ", lt.size(cont['lstTaxi']))
+
+        #ordenar por Numero de taxis
+        controller.ordenar(cont,1)
+        print("")
+        print(m, " Compañias con mas taxis afiliados:\n")
+
+        itera = it.newIterator(cont['lstTaxi'])
+        i=1
+        while it.hasNext(itera):
+            elemento = it.next(itera)
+            print("Nombre de la Compañia  : ",elemento['company_name'])
+            print("Numero Taxis Afiliados: ", elemento['n_taxis'])
+            print("")
+            if (i >= m):
+                break
+            i = i+1
+
+        
+        #Ordenar por Numero de servicios
+        controller.ordenar(cont,2)
+        print("")
+        print(n, " Compañias con mas Servicios realiados:\n")
+
+        itera = it.newIterator(cont['lstTaxi'])
+        i=1
+        while it.hasNext(itera):
+            elemento = it.next(itera)
+            print("Nombre de la Compañia  : ",elemento['company_name'])
+            print("Numero de Servicios : ",elemento['n_servicios'])
+            print("")
+            if (i >= n):
+                break
+            i = i+1
+
+
+    elif int(inputs[0]) == 4:
+        print('N taxis con mas puntos en una fecha ')
+        
+        f = input("Indique Fecha  (YYYY-MM-DD): ")
+        n = int(input("Numero de Taxis "))
+        lst = controller.puntosPorFecha(cont, f,n)
+        for ele in lst:
+            print("taxi id : ", ele['Taxi'])
+            print("Puntos : ", round(ele['puntos'],2))
+            print("")
+
+       
+    
+    elif int(inputs[0]) == 5:
+        print('M taxis con mas puntos en un rango de fechas ')
+        
+        f1 = input("Indique Fecha inicial  (YYYY-MM-DD): ")
+        f2 = input("Indique Fecha final    (YYYY-MM-DD): ")
+        n = int(input("Numero de Taxis "))
+
+        lst = controller.puntosPorRangoFecha(cont, f1,f2, n)
+        for ele in lst:
+            print("taxi id : ", ele['Taxi'])
+            print("Puntos : ", ele['puntos'])
+            print("")
+
+        
+       
+    
+    elif int(inputs[0]) == 6:
+        
+        stacion_ini = "28"
+
+
+        # Rutas de costo minimo de estacion inicial a demas estaciones
+
+        paths = controller.minimumCostPaths(cont, stacion_ini) 
+
+        stacion_fin = "56"
+        #Ruta de costo minimo entre estacion inicial y estacion final
+        path = controller.minimumCostPath(paths, stacion_fin)
+
+        ruta =[]
+
+        if path is not None:
+            pathlen = stack.size(path) #Porque viene en forma de pila
+            print('El camino es de longitud: ' + str(pathlen))
+            while (not stack.isEmpty(path)):
+                stop = stack.pop(path)
+                print(stop)
+                ruta.append(stop['vertexA'] + "-" + stop['vertexB'])
+        else:
+            print('No hay camino')
+
+        hora_ini = int('80')
+        hora_fin = int('1230')
+
+        lst = controller.rutasPorRangoHora(cont, hora_ini,hora_fin,ruta)
+
+        print("mejor Horario :")
+
+        print(lst)
+        
+
+    else:
+        sys.exit(0)
+sys.exit(0)
